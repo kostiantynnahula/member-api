f95b299fe7164e39ab874043f6b20408bc3c49d6
Kostia Nahula <kostyannagula@gmail.com>
2022-12-10 22:44:05 +0200

    Update get organization and create organization methods

--- src/organizations/organizations.resolver.ts
+++ src/organizations/organizations.resolver.ts
@@ -1,10 +1,13 @@
-import { UseGuards } from '@nestjs/common';
+import { BadRequestException, NotFoundException, UseGuards } from '@nestjs/common';
 import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { Organization } from './models/organization.model';
 import { OrganizationInput } from './inputs/organization.input';
 import { OrganizationsService } from './organizations.service';
 import { UsersService } from './../users/users.service';
 import { JwtAuthGuard } from './../auth/guards/jwt-auth.guard';
+import { Auth } from './../auth/auth.decorator';
+import { User } from './../users/models/user.model';
+import { lastValueFrom } from 'rxjs';
 @UseGuards(JwtAuthGuard)
 @Resolver((of) => Organization)
 export class OrganizationsResolver {
@@ -16,15 +19,34 @@ export class OrganizationsResolver {
   ) {}
 
   @Query(() => Organization)
-  async organization() {
-    return this.service.getOrganization(this.organizationId);
+  async organization(@Auth() auth: User) {
+    const organization = await lastValueFrom(
+      this.service.getOrganizationByCreator(auth._id),
+    );
+
+    if (organization) {
+      return new NotFoundException();
+    }
+
+    return organization;
   }
 
   @Mutation(() => Organization, {
     name: 'registerOrganization',
   })
-  async register(@Args('organizationInput') body: OrganizationInput) {
-    return this.service.createOrganization(body);
+  async register(
+    @Args('organizationInput') body: OrganizationInput,
+    @Auth() auth: User,
+  ) {
+    const existedOrganization = await lastValueFrom(
+      this.service.getOrganizationByCreator(auth._id),
+    );
+
+    if (existedOrganization) {
+      return new BadRequestException('You already have an organization');
+    }
+
+    return this.service.createOrganization({ ...body, creator_id: auth._id });
   }
 
   @Mutation(() => Organization, {
--- src/organizations/organizations.service.ts
+++ src/organizations/organizations.service.ts
@@ -1,7 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { OrganizationInput } from './inputs/organization.input';
-
+import { Organization } from './models/organization.model';
 @Injectable()
 export class OrganizationsService {
   constructor(
@@ -18,7 +18,17 @@ export class OrganizationsService {
     );
   }
 
-  createOrganization(data: OrganizationInput) {
+  getOrganizationByCreator(creator_id: string) {
+    return this.client.send<Organization>(
+      {
+        entity: 'organization',
+        cmd: 'get-by-creator',
+      },
+      creator_id,
+    );
+  }
+
+  createOrganization(data: OrganizationInput & { creator_id: string }) {
     return this.client.send(
       {
         entity: 'organization',
